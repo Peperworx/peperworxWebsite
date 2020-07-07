9172c97adc27d915ea9da6b5841ef0fe3cf0c946
riley <meetingprince34@outlook.com>
2020-07-06 21:58:35 -0400

    reset jinja options to default

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import requests
 
 class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
-    jinja_options.update(dict(
-    block_start_string='$$',
-    block_end_string='$$',
-    variable_start_string='$',
-    variable_end_string='$',
-    comment_start_string='$#',
-    comment_end_string='#$',
-    ))
+    #jinja_options.update(dict(
+    #block_start_string='$$',
+    #block_end_string='$$',
+    #variable_start_string='$',
+    #variable_end_string='$',
+    #comment_start_string='$#',
+    #comment_end_string='#$',
+    #))
 
 config = json.load(open("../config.json","r"))
 
